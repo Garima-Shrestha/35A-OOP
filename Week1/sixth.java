2afb4c49ef626dc18b301e753531733aa8fc816d
garima shrestha <garimashrestha20@gmail.com>
2024-04-11 10:56:14 +0545

    6th que completed

---
+++ Week1/sixth.java
@@ -0,0 +1,9 @@
+package task.Week1;
+
+public class sixth {
+    public static void main(String[] args) {
+        System.out.println("A well-formed Java program has a main method with { and } braces.\n");
+        System.out.println("A System.out.println statement has ( and ) and usually a String \nthat starts and ends with a \" character. \n(But we type \\\" instead!) \nWhat is the difference between \na \' and a \"?  Or between a \" and a \\\"?\n ");
+        System.out.println("One is what we see when we're typing our program. The other is what appears on the \"console.\"");
+    }
+}

3c37db476e3deeb667f16f0d3fe5daa43cd7c8db
garima shrestha <garimashrestha20@gmail.com>
2024-04-11 10:23:21 +0545

    2nd que completed

---
+++ Week1/second.java
@@ -0,0 +1,7 @@
+package task.Week1;
+
+public class second {
+    public static void main(String[] args) {
+        System.out.print("This is my first program \nI am on module ST4003CEM");      
+    }
+}

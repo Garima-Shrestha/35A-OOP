65fc0f1df53c8ee0c95dcd431c4b359530aa07f9
garima shrestha <garimashrestha20@gmail.com>
2024-04-11 10:44:39 +0545

    3rd que completed

---
+++ Week1/third.java
@@ -0,0 +1,7 @@
+package task.Week1;
+
+public class third {
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" String is \n\'much\' better if you learn \nthe rules of \"escape sequences.\" \nAlso, \"\" represents an empty \nString. Don't forget: use \\\" \ninstead of \" !\n \" is not the same as \"");
+    }
+}

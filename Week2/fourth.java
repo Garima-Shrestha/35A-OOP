46a57f9195e3fc5b4220e3fd6baa2bb887bf386f
garima shrestha <garimashrestha20@gmail.com>
2024-04-29 12:58:07 +0545

    4th que completed

---
+++ Week2/fourth.java
@@ -0,0 +1,15 @@
+package task.Week2;
+
+import java.util.Scanner;
+
+public class fourth {
+public static void main(String[] args) {
+    /*Write a program to check whether a person can cast a vote or not. The condition is you must be over 18 years old to vote. */
+    /* Write the ternary operator for question no. 1 */
+
+    int validAge=18;
+    int age=21;
+    String result= validAge<age ? "You are eligible to vote":"You are not eligible to vote";
+    System.out.println(result);
+    }
+}

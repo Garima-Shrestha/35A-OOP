85aa7766667303afe914ce6be17c28dde04b16f5
garima shrestha <garimashrestha20@gmail.com>
2024-04-29 15:32:32 +0545

    9th que completed

---
+++ Week2/ninth.java
@@ -0,0 +1,16 @@
+package task.Week2;
+
+import java.util.Scanner;
+
+public class ninth {
+    public static void main(String[] args) {
+        /* Ask user to give two double input for length and breadth of a rectangle and print area type casted to int.*/
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter length of rectangle: ");
+        double length=scan.nextDouble();
+        System.out.println("Enter breadth of rectangle: ");
+        double breadth=scan.nextDouble();
+        int area=(int) (length*breadth);
+        System.out.println("Area of rectangle: "+ area );
+    }
+}

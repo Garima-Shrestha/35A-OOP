12fa4324a4d84993ae01c63c220954be255f9a06
garima shrestha <garimashrestha20@gmail.com>
2024-04-29 16:07:53 +0545

    1st que completed

--- Week2/first.java
+++ Week2/first.java
@@ -1,13 +1,9 @@
 package task.Week2;
 
-import java.util.Scanner;
-
 public class first {
     public static void main(String[] args) {
         /*Write a program to check whether a person can cast a vote or not. The condition is you must be over 18 years old to vote. */
-        Scanner scan=new Scanner(System.in);
-        System.out.println("Enter your age: ");
-        int vote=scan.nextInt();
+        int vote=12;
         if (vote>=18){
                 System.out.println("You can vote");
         }else{

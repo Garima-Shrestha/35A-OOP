83f9d5b7e4dadc4c24bf34533568fe9049af7650
garima shrestha <garimashrestha20@gmail.com>
2024-04-29 13:05:49 +0545

    5th que completed

---
+++ Week2/fifth.java
@@ -0,0 +1,18 @@
+package task.Week2;
+
+import java.util.Scanner;
+
+public class fifth {
+    public static void main(String[] args) {
+        /* Write a program to take two integer inputs from the user and print the sum and product of them. */
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter first integer: ");
+        int num1=scan.nextInt();
+        System.out.println("Enter second integer: ");
+        int num2=scan.nextInt();
+        int sum=num1+num2;
+        System.out.println("Sum of two integers: "+sum);
+        int product=num1*num2;
+        System.out.println("Product of two integers: "+product);
+    }
+}
--- Week2/fourth.java
+++ Week2/fourth.java
@@ -1,7 +1,5 @@
 package task.Week2;
 
-import java.util.Scanner;
-
 public class fourth {
 public static void main(String[] args) {
     /*Write a program to check whether a person can cast a vote or not. The condition is you must be over 18 years old to vote. */
